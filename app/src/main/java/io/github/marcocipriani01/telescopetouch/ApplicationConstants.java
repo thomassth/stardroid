af4d41301d1912de1e224eadb6fd3b751d464a46
Marco Cipriani <marco.cipriani.01@gmail.com>
2021-01-12 16:42:41 +0100

    Update license

--- app/src/main/java/io/github/marcocipriani01/telescopetouch/ApplicationConstants.java
+++ app/src/main/java/io/github/marcocipriani01/telescopetouch/ApplicationConstants.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Marco Cipriani (@marcocipriani01) and the Sky Map Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.marcocipriani01.telescopetouch;
 
 import io.github.marcocipriani01.telescopetouch.units.Vector3;
@@ -26,8 +42,6 @@ public class ApplicationConstants {
     public static final String BUNDLE_Y_TARGET = "bundle_y_target";
     public static final String BUNDLE_Z_TARGET = "bundle_z_target";
     public static final String BUNDLE_SEARCH_MODE = "bundle_search";
-    // Preference that keeps track of whether or not the user accepted the ToS for this version
-    public static final String READ_TOS_PREF_VERSION = "read_tos_version";
     public static final String SHARED_PREFERENCE_DISABLE_GYRO = "disable_gyro";
     public static final String REVERSE_MAGNETIC_Z_PREFKEY = "reverse_magnetic_z";
     public static final String ROTATE_HORIZON_PREFKEY = "rotate_horizon";
