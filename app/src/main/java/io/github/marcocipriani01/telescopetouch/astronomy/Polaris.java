8a49c95517c766246a1475808f4e109fd8d15bad
Marco Cipriani <marco.cipriani.01@gmail.com>
2021-01-30 18:32:27 +0100

    Fixes

---
+++ app/src/main/java/io/github/marcocipriani01/telescopetouch/astronomy/Polaris.java
@@ -0,0 +1,17 @@
+package io.github.marcocipriani01.telescopetouch.astronomy;
+
+public class Polaris {
+
+    /**
+     * Polaris' right ascension
+     * <p>
+     * 02h 31m 49.09456s
+     */
+    private static final double POLARIS_RA = 30.53030404;
+    /**
+     * Polaris' declination
+     * <p>
+     * +89° 15' 50.7923"
+     */
+    private static final double POLARIS_DEC = 89.26410897;
+}
